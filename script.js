f36d47ca54ef7de02425b4509ff4999f044da3e5
Tarek <tarek90.salim@live.com>
2022-06-11 23:59:47 +0200

    finalized files

--- script.js
+++ script.js
@@ -1,70 +1,76 @@
-const imgContainer = document.getElementById("img-container");
-const loader = document.getElementById("loader");
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
-// Unsplash APIcontainerloader");
-const count = 10;
-const apiKey = 'rR2Q7d5zII0d2lFIgtyiO0o08PBxZP8IVfdU9HXfIi0';
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+// Unsplash API
+const count = 30;
+const apiKey = 'YOUR_API_KEY_HERE';
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
-// Helper function to set attributes
-function setAttributes(element, attribute) {
-    for (const key in attribute) {
-        element.setAttribute(key, attribute[key])
-    }
+// Check if all images were loaded
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
 }
 
+// Helper Function to Set Attributes on DOM Elements
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
 
-
-
-// Create Elements for Links & Photos, then Add to DOM 
+// Create Elements For Links & Photos, Add to DOM
 function displayPhotos() {
-    // Run function for each object in "photosArray"
-    photosArray.forEach((photo) => {
-        // Create <a> to link to Unsplash
-        const item = document.createElement("a");
-        // item.setAttribute("href", photo.links.html);
-        // item.setAttribute("target", "_blanck");
-        setAttributes(item, {
-            href: photo.links.html,
-            target: "_blanck",
-        });
-        
-        // Create <img> for photo
-        const img = document.createElement("img");
-        // img.setAttribute("src", photo.urls.regular);
-        // img.setAttribute("alt", photo.alt_description);
-        // img.setAttribute("title", photo.description);
-        setAttributes(img, {
-            src: photo.urls.regular,
-            alt: photo.alt_description,
-            title: photo.description
-        });
-
-        // Put <img> inside <a>, then put both insdie imgContainer
-        item.appendChild(img);
-        imgContainer.appendChild(item);
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+  // Run function for each object in photosArray
+  photosArray.forEach((photo) => {
+    // Create <a> to link to full photo
+    const item = document.createElement('a');
+    setAttributes(item, {
+      href: photo.links.html,
+      target: '_blank',
     });
+    // Create <img> for photo
+    const img = document.createElement('img');
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    // Event Listener, check when each is finished loading
+    img.addEventListener('load', imageLoaded);
+    // Put <img> inside <a>, then put both inside imageContainer Element
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
 }
-
 
 // Get photos from Unsplash API
 async function getPhotos() {
-    try {
-        const response = await fetch(apiUrl);
-        photosArray = await response.json();
-        displayPhotos();
-    } catch (error) {
-        // Catch Errors Here
-    }
+  try {
+    const response = await fetch(apiUrl);
+    photosArray = await response.json();
+    displayPhotos();
+  } catch (error) {
+    // Catch Error Here
+  }
 }
 
 // Check to see if scrolling near bottom of page, Load More Photos
-window.addEventListener("scroll", () => {
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) {
-        getPhotos();        
-    }
+window.addEventListener('scroll', () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+    ready = false;
+    getPhotos();
+  }
 });
 
 // On Load
